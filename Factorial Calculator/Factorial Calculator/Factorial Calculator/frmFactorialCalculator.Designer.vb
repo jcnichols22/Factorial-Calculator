aec9fcdc14932c9abcc0e8de16c7286c3b7970e4
jcnichols22 <88635926+jcnichols22@users.noreply.github.com>
2022-06-10 22:19:53 -0400

    Add files via upload

---
+++ Factorial Calculator/Factorial Calculator/Factorial Calculator/frmFactorialCalculator.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmFactorialCalculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblHeader = New System.Windows.Forms.Label()
+        Me.lstFactorials = New System.Windows.Forms.ListBox()
+        Me.btnEnterValue = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.mnuFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuClear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblHeader
+        '
+        Me.lblHeader.AutoSize = True
+        Me.lblHeader.Font = New System.Drawing.Font("Bradley Hand ITC", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeader.Location = New System.Drawing.Point(70, 89)
+        Me.lblHeader.Name = "lblHeader"
+        Me.lblHeader.Size = New System.Drawing.Size(445, 60)
+        Me.lblHeader.TabIndex = 0
+        Me.lblHeader.Text = "Factorial Calculator"
+        '
+        'lstFactorials
+        '
+        Me.lstFactorials.Font = New System.Drawing.Font("Bradley Hand ITC", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstFactorials.FormattingEnabled = True
+        Me.lstFactorials.ItemHeight = 26
+        Me.lstFactorials.Location = New System.Drawing.Point(613, 89)
+        Me.lstFactorials.Name = "lstFactorials"
+        Me.lstFactorials.Size = New System.Drawing.Size(120, 316)
+        Me.lstFactorials.TabIndex = 1
+        '
+        'btnEnterValue
+        '
+        Me.btnEnterValue.Font = New System.Drawing.Font("Bradley Hand ITC", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEnterValue.Location = New System.Drawing.Point(158, 209)
+        Me.btnEnterValue.Name = "btnEnterValue"
+        Me.btnEnterValue.Size = New System.Drawing.Size(177, 50)
+        Me.btnEnterValue.TabIndex = 2
+        Me.btnEnterValue.Text = "Enter Value"
+        Me.btnEnterValue.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuFile})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(816, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'mnuFile
+        '
+        Me.mnuFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuClear, Me.mnuExit})
+        Me.mnuFile.Name = "mnuFile"
+        Me.mnuFile.Size = New System.Drawing.Size(37, 20)
+        Me.mnuFile.Text = "&File"
+        '
+        'mnuClear
+        '
+        Me.mnuClear.Name = "mnuClear"
+        Me.mnuClear.Size = New System.Drawing.Size(180, 22)
+        Me.mnuClear.Text = "&Clear"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(180, 22)
+        Me.mnuExit.Text = "E&xit"
+        '
+        'frmFactorialCalculator
+        '
+        Me.AcceptButton = Me.btnEnterValue
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Factorial_Calculator.My.Resources.Resources._5529077
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(816, 535)
+        Me.Controls.Add(Me.btnEnterValue)
+        Me.Controls.Add(Me.lstFactorials)
+        Me.Controls.Add(Me.lblHeader)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmFactorialCalculator"
+        Me.Text = "Factorial Calculator"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHeader As Label
+    Friend WithEvents lstFactorials As ListBox
+    Friend WithEvents btnEnterValue As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents mnuFile As ToolStripMenuItem
+    Friend WithEvents mnuClear As ToolStripMenuItem
+    Friend WithEvents mnuExit As ToolStripMenuItem
+End Class
---
+++ Factorial Calculator/Factorial Calculator/Factorial Calculator/frmFactorialCalculator.vb
@@ -0,0 +1,59 @@
+﻿' Program Name: Factorial Calculator        
+' Author:       Josh Nichols
+' Date:         March 27, 2022
+' Purpose:      The Factorial Calculator takes input from the user then calculates
+'               the factorials from 1 up to the users selection
+
+Public Class frmFactorialCalculator
+    Private Sub btnEnterValue_Click(sender As Object, e As EventArgs) Handles btnEnterValue.Click
+        ' This event handler takes user input and the using the factorial funtion will calculate the factorial of all numbers 
+        ' from 1 up to the user seclection
+
+        'Variable declartions
+        Dim strUserInput As String
+        Dim intValue As Integer
+        Dim intFactorial As Long
+        Dim intUserInput As Integer
+
+        ' taking user input and assigning it to a variable for use
+        strUserInput = InputBox("Please enter a number from 1 to 12", "Factorial Selection")
+        intUserInput = Convert.ToInt32(strUserInput)
+
+
+        Do While intValue < intUserInput
+
+            'intFactorial = Factorial(intValue)
+
+            intValue += 1
+
+            intFactorial = Factorial(intValue)
+
+            lstFactorials.Items.Add(intValue.ToString() & "! " & intFactorial.ToString())
+        Loop
+
+
+    End Sub
+
+
+    Public Function Factorial(value As Integer) As Long
+        'function to calculate the factorial of a give number
+
+        If value = 0 Then
+            Return 1
+        Else
+            Return value * Factorial(value - 1)
+        End If
+    End Function
+
+    Private Sub mnuClear_Click(sender As Object, e As EventArgs) Handles mnuClear.Click
+        ' clears the Factorials list
+
+        lstFactorials.Items.Clear()
+    End Sub
+
+    Private Sub mnuExit_Click(sender As Object, e As EventArgs) Handles mnuExit.Click
+        ' Closes the program
+
+        Close()
+    End Sub
+End Class
